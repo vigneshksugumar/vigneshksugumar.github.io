199b4a72d29b9583d81663f3b9ef2049f31ce4de
vigneshksugumar <46265036+vigneshksugumar@users.noreply.github.com>
2024-10-17 17:23:32 -0400

    Update HelloUser.js

--- HelloUser.js
+++ HelloUser.js
@@ -28,7 +28,7 @@ export class HelloUser extends LitElement {
   }
 
   render() {
-    return html`<p>Hello ${this.who}, Welcome!<p/>`;
+    return html`<p>Hello ${this.who}, Welcome again!<p/>`;
   }
 }
 
