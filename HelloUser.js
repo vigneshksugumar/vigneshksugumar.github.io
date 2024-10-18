4ad0d4b4ec0f0a7a54f50fcb2ea33d9b2f7ac028
vigneshksugumar <46265036+vigneshksugumar@users.noreply.github.com>
2024-10-18 11:40:47 -0400

    Update HelloUser.js

--- HelloUser.js
+++ HelloUser.js
@@ -32,7 +32,7 @@ export class HelloUser extends LitElement {
   }
 
   render() {
-    return html`<p>Hello ${this.who}, Welcome again 1.3!<p/><p>You have 9 items</p>`;
+    return html`<p>Hello ${this.who}, Welcome again 1.4!<p/><p>You have ${getTotalGroupsUserIs} items</p>`;
   }
 }
 
