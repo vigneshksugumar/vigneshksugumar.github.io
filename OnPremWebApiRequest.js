e07f19b5a02d95ae4328ec4f783d4089864b2cc9
vigneshksugumar <46265036+vigneshksugumar@users.noreply.github.com>
2024-10-19 09:07:01 -0400

    Update OnPremWebApiRequest.js

--- OnPremWebApiRequest.js
+++ OnPremWebApiRequest.js
@@ -1,11 +1,16 @@
-import { html,LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-import { Task, TaskStatus } from '@lit/task';
-// define the component
+import {LitElement, css, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+import {Task, initialState} from 'https://cdn.jsdelivr.net/npm/@lit/task@1.0.1/+esm';
+
 export class OnPremWebApiRequest extends LitElement {
   
   static properties = {
     webApi: {type: String},
   };
+  
+  constructor() {
+    super();
+    this.webApi = 'https://api.sampleapis.com/coffee/hot';
+  }
   
   // return a promise for contract changes.
   static getMetaConfig() {
@@ -23,28 +28,79 @@ export class OnPremWebApiRequest extends LitElement {
     };
   } 
   
-  constructor() {
-    super();
-    this.webApi = '';
-  }
-
-  async function fetchMoviesJSON() {
-    const response = await fetch(`${this.webApi}`);
-    const movies = await response.json();
-    return movies;
-  }    
+  private _getApiDataTask = new Task(this, {
+    task: async ([webApiUrl], {signal}) => {      
+      const resp = await fetch(`${webApiUrl}`);
+      const respJson = resp.json();
+      return respJson;
+    },
+    args: () => [this.webApi],
+  });
 
   render() {
-    fetchMoviesJSON().then(user => {      
-      return html`
-      <p>Data</p>
-      <p>${user.length}</p>      
+    return html`
+      <label>
+        Enter a url:
+        <input .value=${this.webApi} @change=${this._onChange} />
+      </label>
+        <br /><br />
+      <header>
+        <b>${this.webApi}</b>       
+      </header>
+      <div>
+        ${this._getApiDataTask.render({
+          initial: () =>
+            html`<span class="initial">
+              Loading...
+            </span>`,
+          pending: () =>
+            html`<p>Loading data for ${this.webApi}</p>`,
+          complete: (items) => html`            
+            <ul>
+              ${items.map(
+                (item, index) =>
+                html`<li>${index}: ${item.title}</li>`
+              )}
+            </ul>
+          `,
+          error: (e) => html`<span class="error">
+            Error: ${(e as Error).message}
+              </span>`,
+        })}
+      </div>
     `;
-    });    
+  }  
+
+  private _onChange(e: Event) {
+    this.webApi = (e.target as HTMLInputElement).value;
   }
-  
+
+  static styles = css`
+    :host {
+      display: block;
+      min-width: 300px;
+      border-radius: 5px;
+      border: solid 1px #aaa;
+      padding: 20px;
+    }
+    header {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    }
+    #logo {
+      height: 38px;
+      width: auto;
+    }
+    .initial {
+      font-style: italic;
+    }
+    .error {
+      color: red;
+    }
+  `;
 }
 
-// registering the web component
 const elementName = 'onprem-webapi-request';
 customElements.define(elementName, OnPremWebApiRequest);
