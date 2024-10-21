3581136ddd8b989fa1f6789f93fc24ff8ce42573
vigneshksugumar <vigneshks@hotmail.com>
2024-10-20 22:18:16 -0400

    v2

--- HelloUser.js
+++ HelloUser.js
@@ -44,7 +44,7 @@ export class HelloUser extends LitElement {
 
   render() {
     return html`
-        <p>Hello ${this.who}, Welcome again 1.20!<p/>  
+        <p>Hello ${this.who}, Welcome again 1.21!<p/>  
         <input id="simpleInput" type="text" value="${this.who}" @change=${() => this.onChange()} />        
         `;
   }
