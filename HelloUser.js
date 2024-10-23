330f32e0a23989ad99b62029aeb5eb2b20b1c866
vigneshksugumar <vigneshks@hotmail.com>
2024-10-23 12:49:49 -0400

    Dev Commit

--- HelloUser.js
+++ HelloUser.js
@@ -1,28 +1,39 @@
-import {LitElement, html} from 'lit';
-export class MyPlugin extends LitElement {
+import { html,LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
+export class HelloUser extends LitElement {
+  
   static properties = {
-    message: {},
+    who: {type: String},
   };
-
-  static styles = css`
-    .highlight {
-      font-weight: 700;
-      color: #E8960A;
-  }
-`;
-
+  
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Hello User',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
+        who: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to today'          
+        }
+      }
+    };
+  } 
+  
   constructor() {
     super();
-    this.message = 'Hello World';
+    this.who = 'User1';
   }
 
   render() {
     return html`
-      <div id="pluginDiv">
-        <p><input type="text" value="${this.message}" /></p>
-        <p>This is my message ${this.message}.</p>
-      </div>
-	`;
+        <p>Hello ${this.who}, Welcome again 1.33!</p>
+        <input id="simpleInput" type="text" value="${this.who}" />        
+        `;
   }
 }
-customElements.define('hello-user', MyPlugin);
+
+
+customElements.define('hello-user', HelloUser);
