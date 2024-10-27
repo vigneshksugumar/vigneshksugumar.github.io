190b1c3d5649fbd68fc42a5478517f29f7206798
vigneshksugumar <vigneshks@hotmail.com>
2024-10-27 16:10:24 -0400

    Dev Commit

--- webapi-request.js
+++ webapi-request.js
@@ -56,7 +56,7 @@ export class OnPremWebApiRequest extends LitElement {
             defaultValue: 'string'
         },
         outcome: {
-          title: 'Rating',
+          title: 'Outcome',
           type: 'string',
         	description: 'Outcome of WebApi',
           isValueField: true        
